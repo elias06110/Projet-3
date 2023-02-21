c33d09a5eba08a65477a72a5559e10e1b7f815f9
elias06110 <elias06110@gmail.com>
2023-02-21 23:15:23 +0100

    debut page de connexion

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -16,8 +16,12 @@ function addfigure(works, sectionGallery) {
   sectionGallery.append(figure);
 }
 
+
+
+
 /*fonction qui appelle l'api et affiche les figures 
 dans la gallerie en appelant la fonction addfigure*/
+
 
 async function displayfigure() {
   await fetch("http://localhost:5678/api/works")
@@ -25,43 +29,123 @@ async function displayfigure() {
     .then((works) => {
       const sectionGallery = document.querySelector(".gallery");
       works.forEach(works => {addfigure(works, sectionGallery);
-       
       });
+     
     });
 }
 
 displayfigure();
 
-
-
-/* fonction pour ajouter filtre*/ 
-
-
-function addfilter(categories,sectionCategories){
-
-  let Button= document.createElement('button')
-  
-  Button.appendChild(document.createTextNode(categories.name))
-  
-  sectionCategories.appendChild(Button);
-}
-
-
+/* fonction qui affiche les filtres */ 
 
 async function displayFilter(){
   await fetch ("http://localhost:5678/api/categories")
   .then((data)=>data.json())
-  .then((categories)=>{
-    const sectionCategories= document.querySelector('.container')
-    categories.forEach(categories=>{
-      addfilter(categories,sectionCategories)      
+  .then((category)=>{
+   
+   category.forEach(category=>{
+      addfilter(category.name)      
     })
-
-  console.log(categories)
+    
     
 })}
+
 
 displayFilter()
 
 
+/* fonction qui ajoute les filtres */ 
 
+
+function addfilter(name){
+
+  const filter = document.getElementById("filters_category")
+  
+  
+  
+  let button =   document.createElement("button")
+
+  button.classList.add("filters")
+
+  button.type= "button"
+
+  button.append(document.createTextNode(name))
+
+  filter.append(button)
+  
+  
+
+  
+  /* bouttons de filtres*/ 
+
+  
+  button.addEventListener("click",function(){
+    fetch ("http://localhost:5678/api/works")
+    .then((data)=>data.json())
+    .then((works)=>{
+      const WorksFilter = works.filter(function(works){
+        return works.category.name === name
+      })
+      
+      const sectionGallery = document.querySelector(".gallery");
+
+      document.querySelector(".gallery").innerHTML=""
+      
+      WorksFilter.forEach(WorksFiltre=>{addfigure(WorksFiltre,sectionGallery, works)})
+    
+    })  
+})}
+
+
+/* Boutton "tous */
+
+
+const ButtonAll = document.querySelector(".filters")
+
+ButtonAll.append(document.createTextNode("Tous"))
+
+ButtonAll.addEventListener('click',function(){
+  
+  document.querySelector(".gallery").innerHTML=""
+
+  displayfigure()
+ 
+})
+
+
+/* page de connexion*/ 
+
+  const log = document.querySelector('.login')
+
+  log.addEventListener('click',function(){
+    
+    window.location="login.html"
+})
+
+
+const pro = document.querySelector('.projects')
+
+pro.addEventListener('click',function(){
+  
+  window.location="index.html"
+
+})
+
+
+  function login(){
+    const connect = document.querySelector('connexion')
+
+    connect.addEventListener('click',function(){
+      console.log('hello')
+    })
+  }
+
+  
+   
+  
+
+
+ 
+  
+
+ 
